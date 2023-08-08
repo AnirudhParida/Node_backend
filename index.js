234142fec67950252f26895d6b10d5666ab0a4db
Anirudh Parida <86073238+AnirudhParida@users.noreply.github.com>
2023-08-08 23:23:56 +0530

    ESJ and Express

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+const port = 3000;
 
+//Middleware
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
+
+const users=[]
+
+//setting static folder
+// app.get("/", (req, res) => {
+//     res.sendFile("index.html");
+// });
+
+//setting view engine
+app.set("view engine", "ejs");
+
+//rendering html file
+app.get("/inputs", (req, res) => {
+    res.render("index");
+});
+app.get("/inputs", (req, res) => {
+    res.redirect("details");
+});
+
+//sending json data
 app.get("/getjson", (req, res) => {
     res.json({ message: "Hello World" , subjects: ["Maths", "Science", "English"]});
+});
+
+//posting user data
+app.post('/details', (req, res) => {
+    const newUser = { name: req.body.name, email: req.body.email };
+    users.push(newUser);
+    res.render('details', {  });
 });
 
 app.listen(3000,()=> {
